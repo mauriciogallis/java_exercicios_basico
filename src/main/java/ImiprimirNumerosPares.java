adccead63b60308305961538f6badda08699fd6d
Mauricio Gallis Sahao <mauriciogallis@gmail.com>
2021-03-05 09:26:19 -0300

    acerto de exercicios

--- src/main/java/ImiprimirNumerosPares.java
+++ src/main/java/ImiprimirNumerosPares.java
@@ -5,10 +5,5 @@ public class ImiprimirNumerosPares {
      */
 
     public static void main(String[] args) {
-        for(int i =1; i < 101; i++){
-            if( i % 2 == 0){
-                System.out.println(i);
-            }
-        }
     }
 }
